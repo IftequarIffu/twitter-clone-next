b491dbc09bae3a974688b0d94db5da19bc7246a5
Iftequar Ahmed <iftequar.154@gmail.com>
2023-12-20 08:07:23 +0530

    Added a 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='w-6/12 border-l border-r flex border-zinc-700 h-full min-h-screen justify-center items-center'>
+      <h1 className='font-semibold'>404 | Page Not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
