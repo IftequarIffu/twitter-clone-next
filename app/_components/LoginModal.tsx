a52d39093efd92d4141ff13b28dcbff5254eaae4
Iftequar Ahmed <iftequar.154@gmail.com>
2023-12-22 12:50:01 +0530

    Fixed the Hydration error

--- app/_components/LoginModal.tsx
+++ app/_components/LoginModal.tsx
@@ -18,6 +18,8 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
 
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+
+  const [screen, setScreen] = useState("signin")
   
   const handleInputClick = (event: React.MouseEvent<HTMLInputElement>) => {
     // Stop the click event propagation to prevent the modal from closing
@@ -85,9 +87,11 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
             onChange={(e) => setPassword(e.target.value)}
             onClick={handleInputClick}
           ></input>
-          <button className="bg-white  text-black px-4 py-2 rounded-xl w-full font-semibold hover:bg-gray-200 " onClick={(e) => loginMethod(e)}>
-              Sign In
+          
+          <button className="bg-white text-black px-4 py-2 rounded-xl w-full font-semibold hover:bg-gray-200 " onClick={(e) => loginMethod(e)}>
+              <h1>Sign In</h1>
           </button>
+
           <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-xl text-sm px-5 py-2.5 text-center items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2 flex justify-center w-full">
             <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
             <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
@@ -95,9 +99,12 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
             Sign in with Google
           </button>
             <div>
-              <p className="mt-6 text-gray-400">Don&apos;t have an account? 
-              <Link href="/signup" className="text-primary hover:underline ms-2">Sign Up</Link>
-              </p>
+              <div className="mt-6 text-gray-400"><div className="inline">Don&apos;t have an account? </div>
+              <div className="text-primary hover:underline ms-2 inline hover:cursor-pointer"
+               onClick={(e) => {e.stopPropagation(); setScreen("signup") }}>
+                Sign Up
+              </div>
+              </div>
             </div>
           </div>
         </div>
